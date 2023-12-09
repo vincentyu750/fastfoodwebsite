b40f2c1662a967ec2193358169952c5041426571
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-12-08 19:36:48 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
         {/* Navigation links */}
         <ul className="flex text-white">
           <li className="mr-6">
-            <Link href="/index">Home</Link>
+            <Link href="/">Home</Link>
           </li>
           <li>
             <Link href="/about">About</Link>
